477241f93b031e224158d6b05a59b210c8453c19
keefereuther <kdreuther@ucsd.edu>
2024-04-07 10:22:57 -0700

    password protection

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import hmac
 import pandas as pd
 import random
 import os
@@ -8,6 +9,32 @@ import logging
 import io
 import config
 from openai import OpenAI
+
+def check_password():
+    """Returns `True` if the user had the correct password."""
+
+    def password_entered():
+        """Checks whether a password entered by the user is correct."""
+        if hmac.compare_digest(st.session_state["password"], st.secrets["password"]):
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]  # Don't store the password.
+        else:
+            st.session_state["password_correct"] = False
+
+    # Return True if the password is validated.
+    if st.session_state.get("password_correct", False):
+        return True
+
+    # Show input for password.
+    st.text_input(
+        "Password", type="password", on_change=password_entered, key="password"
+    )
+    if "password_correct" in st.session_state:
+        st.error("😕 Password incorrect")
+    return False
+
+if not check_password():
+    st.stop()  # Do not continue if check_password is not True.
 
 # Set up logging
 logging.basicConfig(level=logging.DEBUG, filename='app_log.log', filemode='w',
