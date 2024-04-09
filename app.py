154eda0024a344be0a5964807f176ed6d241d3e7
keefereuther <kdreuther@ucsd.edu>
2024-04-09 12:15:21 -0700

    huge updates to UI

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
+
+############################################################################################################
+# Importing Libraries
+
 import streamlit as st
 import hmac
 import pandas as pd
@@ -9,6 +13,9 @@ import logging
 import io
 import config
 from openai import OpenAI
+
+############################################################################################################
+# Password protection
 
 def check_password():
     """Returns `True` if the user had the correct password."""
@@ -36,12 +43,43 @@ def check_password():
 if not check_password():
     st.stop()  # Do not continue if check_password is not True.
 
-# Set up logging
+############################################################################################################
+# Logging
+
 logging.basicConfig(level=logging.DEBUG, filename='app_log.log', filemode='w',
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
-# Creating a download link for a file
-# https://discuss.streamlit.io/t/how-to-download-file-in-streamlit/1806/2
+############################################################################################################
+# Streamlit app layout
+
+# Set the page to wide or centered mode
+st.set_page_config(layout="centered")
+
+# Streamlit app layout
+st.title(config.app_title)
+st.caption(config.app_author)
+# st.markdown(config.intro_para)
+with st.expander("INSTRUCTIONS:"):
+    st.markdown(config.instructions)
+st.sidebar.title(config.sidebar_title)
+
+############################################################################################################
+# File Uploader in sidebar
+
+# Load terms from a CSV file
+# https://discuss.streamlit.io/t/how-to-upload-a-csv-file/7052/2
+def load_terms(file_input):
+    try:
+        if isinstance(file_input, str):
+            data = pd.read_csv(file_input)
+        else:
+            data = pd.read_csv(io.StringIO(file_input.read().decode('utf-8')))
+        return data
+    except Exception as e:
+        st.error(f"An error occurred while loading the file: {str(e)}")
+        logging.exception(f"Error loading file: {e}")
+
+# Function to create a download link for a file
 def create_download_link(file_path, file_name):
     try:
         with open(file_path, "rb") as file:
@@ -58,56 +96,11 @@ def create_download_link(file_path, file_name):
         st.error(error_message)
         logging.exception(error_message)
 
-# Load terms from a CSV file
-# https://discuss.streamlit.io/t/how-to-upload-a-csv-file/7052/2
-def load_terms(file_input):
-    try:
-        if isinstance(file_input, str):
-            data = pd.read_csv(file_input)
-        else:
-            data = pd.read_csv(io.StringIO(file_input.read().decode('utf-8')))
-        return data
-    except Exception as e:
-        st.error(f"An error occurred while loading the file: {str(e)}")
-        logging.exception(f"Error loading file: {e}")
-
-# Set the page to wide mode
-st.set_page_config(layout="wide")
-
-# Set custom styles
-def set_custom_styles():
-    st.markdown("""
-    <style>
-    /* Custom font size for specific markdown lines */
-    .markdown-font-large {
-        font-size: 14pt;  /* Adjust the size as needed */
-    }
-
-    /* [Other styles] */
-    </style>
-    """, unsafe_allow_html=True)
-
-set_custom_styles()
-
-# Streamlit app layout
-# col1, col2 = st.columns([1,5])
-st.title(config.app_title)
-st.write(config.app_author)
-st.markdown(config.intro_para)
-st.markdown('<br>', unsafe_allow_html=True)
-st.sidebar.title(config.sidebar_title)
-
-# Set a seed for random number generation
-random_seed = 25
-random.seed(random_seed)
-
 # Download link for the template file
 template_file_path = "terms_template.csv"
-st.sidebar.markdown("If you want to use your own terms, download this template file. Be sure to save the file as a CSV and not edit the first row.")
-st.sidebar.markdown(create_download_link(template_file_path, "terms_template.csv"), unsafe_allow_html=True)
 
 # File Uploader
-uploaded_file = st.sidebar.file_uploader("Choose a text file with terms")
+uploaded_file = st.sidebar.file_uploader("Upload a CSV file with terms", type=["csv"])
 if uploaded_file is not None:
     logging.info(f"File uploaded: {uploaded_file.name}")
     st.session_state.uploaded_file = uploaded_file
@@ -118,10 +111,18 @@ if 'uploaded_file' in st.session_state and st.session_state.uploaded_file is not
 else:
     terms = load_terms(template_file_path)
 
+st.sidebar.markdown(create_download_link(template_file_path, "terms_template.csv"), unsafe_allow_html=True)
+
+# line break in the sidebar
+st.sidebar.markdown('<hr>', unsafe_allow_html=True)
+
+############################################################################################################
+# Term Selection and session state
+
 # Function to select a random term and its schema
-def select_random_term_and_schema(terms_df, counter):
+def select_random_term_and_schema(terms_df):
     if not terms_df.empty and 'TERM' in terms_df.columns and 'SCHEMA' in terms_df.columns:
-        random.seed(counter)
+        #random.seed(counter)
         selected_row = terms_df.sample()
         selected_term = selected_row['TERM'].values[0]
         selected_schema = selected_row['SCHEMA'].values[0]
@@ -129,13 +130,6 @@ def select_random_term_and_schema(terms_df, counter):
     else:
         return None, None
     
-
-# Display the instructions
-st.markdown('<div class="markdown-font-large">Click the button below to randomly pick a term</div>', unsafe_allow_html=True)
-
-# Line Break
-st.markdown('<br>', unsafe_allow_html=True)
-
 # Define a basic initial context at the beginning of your script
 initial_context = {
     "role": "system",
@@ -153,15 +147,12 @@ if 'display_messages' not in st.session_state:
 # Initialize session states for the selected term, counter, and display flag
 if 'selected_term' not in st.session_state:
     st.session_state.selected_term = None
-if 'term_click_counter' not in st.session_state:
-    st.session_state.term_click_counter = 0
 if 'display_term' not in st.session_state:
    st.session_state.display_term = False
 
 # Toggle term display and select a new term if needed
 if st.button('Click to pick a term'):
-    st.session_state.term_click_counter += 1
-    selected_term, selected_schema = select_random_term_and_schema(terms, st.session_state.term_click_counter)
+    selected_term, selected_schema = select_random_term_and_schema(terms)
     st.session_state.selected_term = selected_term
     st.session_state.selected_schema = selected_schema
     st.session_state.display_term = True
@@ -181,15 +172,10 @@ if st.session_state.display_term and st.session_state.selected_term:
     # Pass the displayed term to the assistant as part of the message
     user_message = f"Define '{st.session_state.selected_term}':"
 elif not st.session_state.selected_term:
-    st.write("Please click the button to begin")
+    st.write("")
 
-st.markdown('<hr>', unsafe_allow_html=True)
-
-#print("DataFrame:", terms)
-#print("Selected Term:", st.session_state.selected_term)
-
-#####OPENAI CHAT####
-
+############################################################################################################
+# ChatGPT
 # Initialize the OpenAI client
 client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
 
@@ -200,58 +186,71 @@ if "openai_model" not in st.session_state:
 if "display_messages" not in st.session_state:
     st.session_state.display_messages = [initial_context]
 
-# Display the messages
-st.markdown(config.instructions)
+# Get user input
+prompt = st.chat_input("Type your message here...")
 
 # Input for new messages
-if prompt := st.chat_input("Type your message here..."):
-    # Insert initial_context here
-    if not st.session_state.display_messages:
-        st.session_state.display_messages.append(initial_context)
+if prompt:
+    # Ensure the initial context is in the session state, add the user's message
+    if not st.session_state["display_messages"]:
+        st.session_state["display_messages"].append(initial_context)
+    st.session_state["display_messages"].append({"role": "user", "content": prompt})
 
-    st.session_state.display_messages.append({"role": "user", "content": prompt})
-    with st.chat_message("user"):
-        st.markdown(prompt)
+# Main chat container
+with st.container(height=300, border=True):
+    # Display chat history in reverse order including new messages
+    for message in st.session_state["display_messages"][1:]:
+        if message["role"] == "user":
+            with st.chat_message("user"):
+                st.markdown(message["content"])
+        else:
+            with st.chat_message("assistant"):
+                st.markdown(message["content"])
 
-    with st.chat_message("assistant"):
-            message_placeholder = st.empty()
-            full_response = ""
-            for response in client.chat.completions.create(
-                model=st.session_state["openai_model"],
-                messages=[
-                    {"role": m["role"], "content": m["content"]}
-                    for m in st.session_state.display_messages
-                ],
-                stream=True,
-                temperature=config.temperature,
-                max_tokens=config.max_tokens,
-            ):
-                full_response += (response.choices[0].delta.content or "")
-                message_placeholder.markdown(full_response + "▌")
-            message_placeholder.markdown(full_response)
-    st.session_state.display_messages.append({"role": "assistant", "content": full_response})
+# Generate assistant's response and add it to the messages
+    if prompt:
+        # Call the OpenAI API without streaming to get a complete response
+        response = client.chat.completions.create(
+            model=st.session_state["openai_model"],
+            messages=[
+                {"role": m["role"], "content": m["content"]}
+                for m in st.session_state["display_messages"]
+            ],
+            stream=False,  # Disable streaming
+            temperature=config.temperature,
+            max_tokens=config.max_tokens,
+        )
 
-# Line Break
-st.markdown('<br>', unsafe_allow_html=True)
+        # Correctly extract the full response from the API's return object.
+        full_response = response.choices[0].message.content
 
-# Display the messages
-st.markdown('<div class="markdown-font-large"><em>ChatGPT can make errors and does not replace verified and reputable online and classroom resources.</em></div>', unsafe_allow_html=True)
+        # Append the full response to the session state for display.
+        st.session_state["display_messages"].append({"role": "assistant", "content": full_response})
 
-st.sidebar.markdown("Online Resources")
+        # Directly display the assistant's response in the chat container
+        with st.container():
+            st.chat_message("assistant").write(full_response)
+
+st.markdown(config.warning_message, unsafe_allow_html=True)
+
+############################################################################################################
+
+# Resources and About Sections in the Sidebar
+
+st.sidebar.title("Resources")
+
 for resource in config.resources:
     with st.sidebar:
-        st.markdown(f"### [{resource['title']}]({resource['url']})")
-        st.write(resource['description'])
+        with st.expander(resource["title"]):
+            st.markdown(f"Description: {resource['description']}")
+            st.markdown(f"[Link]({resource['url']})")
 
 # Footer
 with st.sidebar:
     st.markdown("---")
 
-    st.markdown("""
-        The template for this app was created by Keefe Reuther and the members of the Reuther Lab - [https://reutherlab.biosci.ucsd.edu/](https://reutherlab.biosci.ucsd.edu/)
-    """)
-    st.markdown("""
-        It can be found at [https://github.com/The-Reuther-Lab/SABER_2024](https://github.com/The-Reuther-Lab/SABER_2024) 
-        and is distributed under the GNU GPL-3 License.
-    """)
+    st.title("About")
 
+   # Using the config objects in your Streamlit app
+    st.markdown(config.app_creation_message, unsafe_allow_html=True)
+    st.markdown(config.app_repo_license_message, unsafe_allow_html=True)
--- config.py
+++ config.py
@@ -1,33 +1,65 @@
 # config.py
-# Don't remove the quotations around the text
-# Don't change the variable names
-app_title = "DIY AI-enhanced study app"
 
-app_author = "Created by Keefe Reuther"
+# THIS IS THE FILE YOU SHOULD EDIT TO CUSTOMIZE THE APP. DO NOT EDIT app.py UNLESS YOU KNOW WHAT YOU ARE DOING.
 
-intro_para = "This is a good place to start if you are just beginning to study for an exam or if you are trying to get a better grasp of the course material. You will be asked to define key terms and concepts from the course. You will receive immediate feedback on your responses."
-
-sidebar_title = "Chat about important course terms and concepts"
-
-initial_prompt = "You are an assistant knowledgeable in university-level biology helping a student in a lower division college course. Provide concise and accurate responses to questions or definitions related to biology questions the user asks. The user will be responding to the following prompt: 'Instructions: First, write a simple definition of some biology term. Include a real-world example and any other related concepts you might need to know for an exam.' Provide formative feedback in a clear, succinct way. Mention any factual errors in the response. Employ the Socratic method, giving the user hints and guiding questions with the goal of getting the user to provide information that was not in the initial user response. Do NOT use extraneous language, such as 'your answer lacks a detailed explanation'. Keep in mind that the user's response is limited to 500 characters, so there is no expectation that the correct answer is more than a short paragraph. Try and keep the system's response within 1000 characters. Make sure to always to provide feedback for each part of the user's input. Do not provide advice, such as: 'Remember, the more specific and detailed your response, the better your understanding of the concept will be.' Your secondary goal as the chat progresses is to help users explicitly think about their learning and study process as well as best practices in information and data literacy. If they write anything unrelated to topics reasonably covered in an undergraduate biology course, please respond with: I appreciate your question, but if you would like to take a break from studying, might I suggest a tall glass of water and mindful relaxation."
-
-# DO NOT REMOVE/EDIT anything outside of the triple quotations or anything inside the curly braces
-# config.py
-def term_prompt(selected_term, selected_schema):
-    return f"""You are an assistant knowledgeable in university-level biology helping a student in a lower division college course. Provide concise and accurate responses to questions or definitions related to the term '{selected_term}'. The user will be responding to the following prompt: 'Instructions: First, write a simple definition of '{selected_term}'. Include a real-world example and any other related concepts you might need to know for an exam.' Provide formative feedback in a clear, succinct way. Base your response on the following definition: '{selected_schema}'. Mention any factual errors in the response. Employ the Socratic method, giving the user hints and guiding questions with the goal of getting the user to provide information that was not in the initial user response. Do NOT use extraneous language, such as 'your answer lacks a detailed explanation'. Keep in mind that my response is limited to 500 characters, so there is no expectation that the correct answer is more than a short paragraph. Try and keep your response within 1000 characters. Make sure to always to provide feedback for each part of the users input. Do not provide advice, such as: 'Remember, the more specific and detailed your response, the better your understanding of the concept will be.' Your secondary goal as the chat progresses is to help users explicitly think about their learning and study process as well as best practices in information and data literacy. If they write anything unrelated to topics possibly covered in an undergraduate biology course, please respond with: I appreciate your question, but if you would like to take a break from studying, might I suggest a tall glass of water and mindful relaxation."""
-
-instructions = "Instructions: First, write a simple definition of the selected term. Include a real-world example and any other related concepts you might need to know for an exam. Please follow-up with questions. Have a conversation!"
+############################################################################################################
 
 # Below is the configuration for the chatbot
 
 # The model_name refers to the name of the model you want to use. You can choose from the following models: 
-ai_model = "gpt-4-1106-preview"
+ai_model = "gpt-4-0125-preview"
 
 # Temperature refers to the randomness/creativity of the responses. A higher temperature will result in more random/creative responses. It varies between 0 and 1.
 temperature = 0
 
 # Max_tokens refers to the maximum number of tokens (words) the AI can generate. The higher the number, the longer the response. It varies between 1 and 2048.
 max_tokens = 500
+
+############################################################################################################
+
+# Below is all the text you can customize for the app. Don't remove the quotations around the text. Don't change the variable names.
+
+# The title of the app
+app_title = "DIY AI-enhanced study app"
+
+# The subtitle of the app
+app_author = "courtesy of UCSD School of Biological Sciences"
+
+# This is an intro paragraph you can add under the title. it is not currently being used in the app.
+intro_para = " "
+
+# The user's instructions for the app
+instructions = '''The goal of this app is to help you learn and and assess your knowledge of core course concepts and examples. 
+1. Click the button below to show a random course term. 
+2. *Pause and think for 30 seconds.* What is everything you associate with this term? 
+3. Choose to either answer immediately or dive into your notes or textbook to refresh your memory.
+4. Write a simple definition of the selected term. Try to include a real-world example and any other associations you might need to know for an exam. 
+5. Please follow-up with questions. **Have a conversation!**'''
+
+# The title of the sidebar
+sidebar_title = "Terms to Upload?"
+
+app_creation_message = "The template for this app was created by Keefe Reuther and the members of the Reuther Lab - [https://reutherlab.biosci.ucsd.edu/](https://reutherlab.biosci.ucsd.edu/)"
+
+app_repo_license_message = "It can be found at [https://github.com/The-Reuther-Lab/SABER_2024](https://github.com/The-Reuther-Lab/SABER_2024) and is distributed under the GNU GPL-3 License."
+
+warning_message = "**ChatGPT can make errors and does not replace verified and reputable online and classroom resources.**"
+
+############################################################################################################
+
+### PROMPTS
+
+# Below is the initial prompt that the AI will use to start the conversation with the user. The user will not see this prompt.
+initial_prompt = "You are an assistant knowledgeable in university-level biology helping a student in a lower division college course. Provide concise and accurate responses to questions or definitions related to biology questions the user asks. The user will be responding to the following instructions set in single quotations: (start of instructions to the user) 'The goal of this app is to help you learn and and assess your knowledge of core course concepts and examples. 1. Click the button below to show a random course term. 2. *Pause and think for 30 seconds.* What is everything you associate with this term? 3. Choose to either answer immediately or dive into your notes or textbook to refresh your memory.4. Write a simple definition of the selected term. Try to include a real-world example and any other associations you might need to know for an exam. 5. Please follow-up with questions. **Have a conversation!**' (end of instructions to the user). Provide formative feedback in a clear, succinct way. Mention any factual errors in the response. Employ the Socratic method, giving the user hints and guiding questions with the goal of getting the user to provide information that was not in the initial user response. Do NOT use extraneous language, such as 'your answer lacks a detailed explanation'. Keep in mind that the user's response is limited to 500 characters, so there is no expectation that the correct answer is more than a short paragraph. Try and keep the system's response within 1000 characters. Make sure to always to provide feedback for each part of the user's input. Do not provide advice, such as: 'Remember, the more specific and detailed your response, the better your understanding of the concept will be.' Your secondary goal as the chat progresses is to help users explicitly think about their learning and study process as well as best practices in information and data literacy. If they write anything unrelated to topics reasonably covered in an undergraduate biology course, please respond with: I appreciate your question, but if you would like to take a break from studying, might I suggest a tall glass of water and mindful relaxation."
+
+# Below is the follow-up prompt that the AI will use once the user has typed a message. The user will not see this prompt.
+# DO NOT REMOVE/EDIT anything outside of the triple quotations or anything inside the curly braces
+def term_prompt(selected_term, selected_schema):
+    return f"""You are an assistant knowledgeable in university-level biology helping a student in a lower division college course. Provide concise and accurate responses to questions or definitions related to the term '{selected_term}'. The user will be responding to the following instructions set in single quotations:(start of instructions to the user) 'The goal of this app is to help you learn and and assess your knowledge of core course concepts and examples. 1. Click the button below to show a random course term. 2. *Pause and think for 30 seconds.* What is everything you associate with this term? 3. Choose to either answer immediately or dive into your notes or textbook to refresh your memory.4. Write a simple definition of the selected term. Try to include a real-world example and any other associations you might need to know for an exam. 5. Please follow-up with questions. **Have a conversation!**' (end of instructions to the user). Provide formative feedback in a clear, succinct way. Base your response on the following definition: '{selected_schema}'. Mention any factual errors in the response. Employ the Socratic method, giving the user hints and guiding questions with the goal of getting the user to provide information that was not in the initial user response. Do NOT use extraneous language, such as 'your answer lacks a detailed explanation'. Keep in mind that my response is limited to 500 characters, so there is no expectation that the correct answer is more than a short paragraph. Try and keep your response within 1000 characters. Make sure to always to provide feedback for each part of the users input. Do not provide advice, such as: 'Remember, the more specific and detailed your response, the better your understanding of the concept will be.' Your secondary goal as the chat progresses is to help users explicitly think about their learning and study process as well as best practices in information and data literacy. If they write anything unrelated to topics possibly covered in an undergraduate biology course, please respond with: I appreciate your question, but if you would like to take a break from studying, might I suggest a tall glass of water and mindful relaxation."""
+
+############################################################################################################
+
+### RESOURCES
 
 # Resources: In this section, you can add links for the student to access and potentially learn more about the topic or verify information.
 # You can add the title of the resource, the URL, and a brief description. To delete or add more resources, follow the same format.
@@ -38,29 +70,9 @@ resources = [
         "description": "A comprehensive guide to the basics of evolution, covering key concepts, history of life, and evolutionary mechanisms."
     },
     {
-        "title": "Understanding Evolution - UC Berkeley",
-        "url": "https://evolution.berkeley.edu/",
-        "description": "A one-stop resource for in-depth information on evolution, designed to enhance understanding of what evolution is and how it works."
-    },
-    {
-        "title": "Khan Academy - Biology",
-        "url": "https://www.khanacademy.org/science/biology",
-        "description": "Offers a wide range of biology topics with easy-to-understand video tutorials and practice exercises for undergraduate students."
-    },
-    {
-        "title": "NCBI Bookshelf - Biology",
-        "url": "https://www.ncbi.nlm.nih.gov/books/",
-        "description": "A collection of biology books and literature available online for free, suitable for in-depth study in various biology areas."
-    },
-    {
         "title": "OpenStax - Biology",
         "url": "https://openstax.org/details/books/biology",
         "description": "Provides free, peer-reviewed, openly licensed textbooks for introductory college and AP-level biology courses."
-    },
-    {
-        "title": "Learn Genetics - Utah",
-        "url": "https://learn.genetics.utah.edu/",
-        "description": "An interactive resource offering educational materials on genetics, bioscience, and health topics. Perfect for students and educators looking for comprehensive genetics and bioscience information."
     },
     {
         "title": "Scitable by Nature Education",
